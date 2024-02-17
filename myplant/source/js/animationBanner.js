bfd357cd5c91d0cc75420fa72439d2f8e02f1081
josevitor555 <koservitor123@gmail.com>
2024-02-17 20:40:26 -0300

    Animation added on banner

---
+++ myplant/source/js/animationBanner.js
@@ -0,0 +1,11 @@
+import 'animate.css';
+
+const elementsToAnimate = document.querySelectorAll('.main-text');
+
+function addAnimationClass() {
+    elementsToAnimate.forEach(element => {
+        element.classList.add('animate__animated', 'animate__flash');
+    })
+}
+
+document.addEventListener('DOMContentLoaded', addAnimationClass);
