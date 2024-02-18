a336e8f6009e86d178d4f19f8b6e52fa74a18cc2
josevitor555 <koservitor123@gmail.com>
2024-02-17 21:11:08 -0300

    Aatualization in header

--- myplant/source/js/animationBanner.js
+++ myplant/source/js/animationBanner.js
@@ -5,7 +5,7 @@ const elementsToAnimate = document.querySelectorAll('.main-text');
 function addAnimationClass() {
     elementsToAnimate.forEach(element => {
         element.classList.add('animate__animated', 'animate__flash');
-    })
+    });
 }
 
 document.addEventListener('DOMContentLoaded', addAnimationClass);
---
+++ myplant/source/js/header.js
@@ -0,0 +1,6 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+  var header = this.document.querySelector("header");
+  header.style.background = "#F9EFDB";
+});
