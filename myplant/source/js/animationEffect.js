8abcf901cde0e3af8b15818772cff7ffa38fde40
josevitor555 <koservitor123@gmail.com>
2024-02-28 09:30:48 -0300

    Animation for center title added

--- myplant/source/js/animationEffect.js
+++ myplant/source/js/animationEffect.js
@@ -1,10 +1,16 @@
 import 'animate.css';
 
 const elementsToAnimate = document.querySelectorAll('.main-text');
+const animationForCenterTitle = document.querySelectorAll('.center-title');
 
 function addAnimationClass() {
+    
     elementsToAnimate.forEach(element => {
         element.classList.add('animate__animated', 'animate__flash');
+    });
+
+    animationForCenterTitle.forEach(titleAnimation => {
+        titleAnimation.classList.add('animate__animated', 'animate__bounceInLeft');
     });
 }
 
